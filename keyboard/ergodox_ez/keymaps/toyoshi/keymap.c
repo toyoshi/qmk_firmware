b4bce02d77d9272f4dbef72cd1a1be25545bdd49
toyoshi <toyosystem@gmail.com>
2016-12-22 17:45:04 +0900

    Misocaボタンを追加

--- keyboard/ergodox_ez/keymaps/toyoshi/keymap.c
+++ keyboard/ergodox_ez/keymaps/toyoshi/keymap.c
@@ -89,7 +89,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_TRNS, KC_F6,   KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_F11,
        KC_TRNS, KC_UP,   KC_7,   KC_8,    KC_9,    KC_ASTR, KC_F12,
                 KC_DOWN, KC_4,   KC_5,    KC_6,    KC_PLUS, KC_TRNS,
-       KC_TRNS, KC_AMPR, KC_1,   KC_2,    KC_3,    KC_BSLS, KC_TRNS,
+       KC_TRNS, KC_AMPR, M(1),   KC_2,    KC_3,    KC_BSLS, KC_TRNS,
                          KC_TRNS,KC_DOT,  KC_0,    KC_EQL,  KC_TRNS,
        KC_TRNS, KC_TRNS,
        KC_TRNS,
@@ -153,6 +153,11 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
           unregister_code(KC_RSFT);
         }
         break;
+	case 1:
+	if (record->event.pressed) {
+	  return MACRO( T(LGUI), D(LSFT), T(M), U(LSFT), T(I), T(S), T(O), T(C), T(A), END  );
+	}
+	break;
       }
     return MACRO_NONE;
 };
